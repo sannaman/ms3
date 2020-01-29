4477eaa64bd74e8d690c3226464563c42e060a5b
sannaman1 <sandeeprao.annamaneni@ge.com>
2020-01-29 16:45:39 +0530

    added webservce

---
+++ src/main/java/com/ms/ms3/SampleWebservice.java
@@ -0,0 +1,18 @@
+package com.ms.ms3;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/ms3")
+public class SampleWebservice {
+
+	@GetMapping("/guest/response")
+	public String getGuestAddress() {
+
+		return "Microservice 3!!";
+	}
+
+}
